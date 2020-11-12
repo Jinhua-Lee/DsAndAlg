e48d8dbaa5929ea16918c12a723b26daf2afd760
Jinhua-Lee <Jinhua-Lee@outlook.com>
2020-11-13 01:59:22 +0800

    单链表操作定义：带头结点的与不带头节点的.

--- Ds/02_SingleLinkedList.cpp
+++ Ds/02_SingleLinkedList.cpp
@@ -2,30 +2,121 @@
 /* 测试单链表的方法*/
 void testSingleLinkedList()
 {
-
+	// 创建带头结点的指针
+	SL_Node* hNode = initList_SLh();
+	// 创建不带头结点的指针
+	SL_Node* SL = initList_SL();
 }
 
-/* 01_单链表——初始化单链表_带头节点*/
-SL_Node* initList_SL(SL_Node& hNode)
+/* 01_单链表——初始化单链表_带头结点*/
+SL_Node* initList_SLh()
 {
-	SL_Node* p = &hNode;
-	p = (SL_Node*)malloc(sizeof(SL_Node));
-	if (!p)
+	// 指向头节点的指针
+	SL_Node* hNode = (SL_Node*)malloc(sizeof(SL_Node));
+	if (!hNode)
 	{
 		return NULL;
 	}
-	p->next = NULL;
-	return p;
+	hNode->next = NULL;
+	return hNode;
 }
 
-/* 02_单链表——初始化单链表_无头节点*/
-SL_Node* initList_SL2(SL_Node* pNode)
+/* 02_单链表——初始化单链表_无头结点*/
+SL_Node* initList_SL()
 {
-	pNode = (SL_Node*)malloc(sizeof(SL_Node));
-	if (!pNode)
+	SL_Node* SL = NULL;
+	return SL;
+}
+
+/* 03_单链表——求单链表长度_带头结点*/
+int listLength_SLh(hNode hNode)
+{
+	// 初始迭代长度
+	int length = 0;
+	// 指向头结点的变量
+	SL_Node* p = hNode.next;
+	while (p)
 	{
-		return NULL;
+		length++;
+		p = p->next;
 	}
-	pNode->next = NULL;
-	return pNode;
+	return length;
+}
+
+/* 04_单链表——求单链表长度_无头结点*/
+int listLength_SL(SL sL)
+{
+	int length = 0;
+	// 从头指针开始迭代
+	SL_Node* p = sL;
+	while (sL)
+	{
+		length++;
+		p = p->next;
+	}
+	return length;
+}
+
+/* 05_单链表——销毁单链表_带头节点*/
+void destroy_SLh(hNode& hNode)
+{
+	// 定义p指针，指向头节点后的待删除结点
+	SL_Node* p;
+	// 当前存在待删除结点
+	while (hNode.next)
+	{
+		// 指向待删除结点
+		p = hNode.next;
+		// 头结点连接到后继结点
+		hNode.next = p->next;
+		// 进行当前结点的删除
+		p->elem = NULL;
+		p->next = NULL;
+		free(p);
+	}
+}
+
+/* 06_单链表——销毁单链表_无头节点*/
+void destroy_SL(SL& sL)
+{
+	// 指针p，指向当前待删除结点
+	SL p;
+	while (sL)
+	{
+		// 每次先指向待删除结点
+		p = sL;
+		// 头指针指向后继节点
+		if (p->next)
+		{
+			sL = p->next;
+		}
+		// 进行删除操作
+		p->elem = NULL;
+		p->next = NULL;
+		free(p);
+	}
+}
+
+/* 07_单链表——尾插法建立单链表_带头节点*/
+SL_Node* tailInsert_SLh(hNode& hNode)
+{
+	return NULL;
+}
+
+/* 08_单链表——尾插法建立单链表_不带头节点*/
+SL_Node* tailInsert_SL(SL& sL)
+{
+	return NULL;
+}
+
+/* 09_单链表——头插法建立单链表_带头结点*/
+SL_Node* headInsert_SLh(hNode& hNode)
+{
+	return NULL;
+}
+
+/* 10_单链表——头插法建立单链表_不带头结点*/
+SL_Node* headInsert_SL(SL& sL)
+{
+	return NULL;
 }
--- Ds/02_SingleLinkedList.h
+++ Ds/02_SingleLinkedList.h
@@ -1,4 +1,3 @@
-#pragma once
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -9,25 +8,35 @@
 // 定义存储的元素类型
 #define ElementType int
 
-/* 定义单链表节点*/
+/* 定义单链表结点*/
 typedef struct SL_Node
 {
 	// 存储的元素
 	ElementType elem;
-	// 指向下一个节点的指针
+	// 指向下一个结点的指针
 	SL_Node* next;
-}hNode, *pNode;
+}hNode, *SL;
 
 // 测试单链表的方法
 void testSingleLinkedList();
 
-// 01_单链表——初始化单链表_带头节点
-SL_Node* initList_SL(SL_Node& hNode);
-// 02_单链表——初始化单链表_无头节点
-SL_Node* initList_SL2(SL_Node* pNode);
-// 03_单链表——头插法建立单链表
-SL_Node* headInsert_SL(SL_Node* pNode);
-// 04_单链表——尾插法建立单链表
-SL_Node* tailInsert_SL(SL_Node* pNode);
-// 05_单链表——求单链表的长度
-int length_SL(SL_Node* pNode);
+// 01_单链表——初始化单链表_带头结点
+SL_Node* initList_SLh();
+// 02_单链表——初始化单链表_无头结点
+SL_Node* initList_SL();
+// 03_单链表——求单链表长度_带头结点
+int listLength_SLh(hNode hNode);
+// 04_单链表——求单链表长度_无头结点
+int listLength_SL(SL sL);
+// 05_单链表——销毁单链表_带头节点
+void destroy_SLh(hNode& hNode);
+// 06_单链表——销毁单链表_无头节点
+void destroy_SL(SL& sL);
+// 07_单链表——尾插法建立单链表_带头节点
+SL_Node* tailInsert_SLh(hNode& hNode);
+// 08_单链表——尾插法建立单链表_不带头节点
+SL_Node* tailInsert_SL(SL& sL);
+// 09_单链表——头插法建立单链表_带头结点
+SL_Node* headInsert_SLh(hNode& hNode);
+// 10_单链表——头插法建立单链表_不带头结点
+SL_Node* headInsert_SL(SL& sL);
